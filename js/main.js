843e3384ab3f2911723df0cb06effe622e332c82
DataSamuraii <80856330+DataSamuraii@users.noreply.github.com>
2021-12-15 00:43:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,31 +2,60 @@ let clickedLetter = 0;
 
 document.addEventListener("keydown", function(event) {
     if (event.code == "KeyA") {
-        clickedLetter = "A";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/A.mp3");
+        audio.play();
     }
     if (event.code == "KeyS") {
-        clickedLetter = "S";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/S.mp3");
+        audio.play();
     }
     if (event.code == "KeyD") {
-        clickedLetter = "D";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/D.mp3");
+        audio.play();
     }
     if (event.code == "KeyF") {
-        clickedLetter = "F";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/F.mp3");
+        audio.play();
     }
     if (event.code == "KeyG") {
-        clickedLetter = "G";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/G.mp3");
+        audio.play();
     }
     if (event.code == "KeyH") {
-        clickedLetter = "H";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/H.mp3");
+        audio.play();
     }
     if (event.code == "KeyJ") {
-        clickedLetter = "J";
-        console.log("The " + clickedLetter + " key is pressed.");
+        let audio = new Audio("sounds/J.mp3");
+        audio.play();
+    }
+
+    if (event.code == "KeyW") {
+        let audio = new Audio("sounds/W.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyE") {
+        let audio = new Audio("sounds/E.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyT") {
+        let audio = new Audio("sounds/T.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyY") {
+        let audio = new Audio("sounds/Y.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyU") {
+        let audio = new Audio("sounds/U.mp3");
+        audio.play();
+    }
+
+    let kbd = document.getElementsByClassName("kbd")
+    kbd.addEventListener("click", adder);
+
+    function adder() {
+        kbd.classList.add('clicked-key');
+        console.log("a")
     }
 });

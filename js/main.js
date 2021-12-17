1455b382a98165d0de13aaf1dfb34e3e52852636
DataSamuraii <80856330+DataSamuraii@users.noreply.github.com>
2021-12-17 12:17:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,23 +5,20 @@ for (let i = 0; i < kbd.length; i++){
     kbd[i].addEventListener("click", function (){
         let audio = new Audio("sounds/" + keyLetter + ".mp3");
         audio.play();
-        kbd[i].classList.add('slide-bottom');
-        kbd[i].style.animation = 'none';
-        kbd[i].offsetHeight; /* trigger reflow */
-        kbd[i].style.animation = null;
+        SetKeyAnimation(kbd[i]);
     });
-}
-for (let i = 0; i < kbd.length; i++) {
-    let keyLetter = kbd[i].innerHTML;
+
     document.addEventListener("keydown", function (event) {
         if (event.code === "Key" + keyLetter) {
-            let keyLetter = kbd[i].innerHTML;
             let audio = new Audio("sounds/" + keyLetter + ".mp3");
             audio.play();
-            kbd[i].classList.add('slide-bottom');
-            kbd[i].style.animation = 'none';
-            kbd[i].offsetHeight; /* trigger reflow */
-            kbd[i].style.animation = null;
+            SetKeyAnimation(kbd[i]);
         }
-    })
+    });
+}
+function SetKeyAnimation(key) {
+    key.classList.add('slide-bottom');
+    key.style.animation = 'none';
+    key.offsetHeight; /* trigger reflow */
+    key.style.animation = null;
 }
